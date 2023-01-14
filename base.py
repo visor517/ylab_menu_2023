78dcea7ec880987014f8d38c59b0117b29ac2c32
visor517 <visor517@yandex.ru>
2023-01-14 17:28:40 +0300

    connected to postgresql

---
+++ base.py
@@ -0,0 +1,11 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+
+from config import DATABASE_URL
+
+
+database = Database(DATABASE_URL)
+metadata = MetaData()
+engine = create_engine(
+    DATABASE_URL,
+)
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+import uvicorn
+
+from base import database
 
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+@app.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
