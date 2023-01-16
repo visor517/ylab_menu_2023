f3f16e8333193fbdf5cd5c764f82234d4f33103c
visor517 <visor517@yandex.ru>
2023-01-16 22:38:20 +0300

    drop table

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,8 @@
 from .tables import menus, sub_menus, dishes
 from .base import metadata, engine
 
+# сбрасываем
+metadata.drop_all(bind=engine)
 
+# создаем таблицы
 metadata.create_all(bind=engine)
--- handlers/menu.py
+++ handlers/menu.py
@@ -1,21 +1,30 @@
 from fastapi import APIRouter, Depends
+from typing import List
 
 from db.repository import MenuRepository
 from .depends import get_menu_repository
+from models import Menu
 
 
 router = APIRouter()
 
-@router.get('/')
+@router.get('/', response_model=List[Menu])
 async def get_menus(
-    menu: MenuRepository = Depends(get_menu_repository),
+    menus: MenuRepository = Depends(get_menu_repository),
     limit: int = 100, 
     offset: int = 0):
-    return menu.get_all(limit=limit, offset=offset)
+    return await menus.get_all(limit=limit, offset=offset)
 
 
-@router.get('/<id>')
-async def get_menu_by_id(
-    menu: MenuRepository = Depends(get_menu_repository),
-    id: int = id):
-    return menu.get_by_id(id=id)
+# @router.get('/<id>')
+# async def get_menu_by_id(
+#     menu: MenuRepository = Depends(get_menu_repository),
+#     id: int = id):
+#     return menu.get_by_id(id=id)
+
+
+@router.post('/', response_model=Menu, status_code=201)
+async def create_menu(
+    menu: Menu,
+    menus: MenuRepository = Depends(get_menu_repository)):
+    return await menus.create(m=menu)
