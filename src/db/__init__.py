33427204a9874fa813196ce4d17a8786638a23e1
visor517 <visor517@yandex.ru>
2026-04-30 23:39:31 +0300

    updated requirements and db init

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,8 +1,2 @@
 from .tables import menus, sub_menus, dishes
-from db.base import metadata, engine
-
-# сбрасываем
-metadata.drop_all(bind=engine)
-
-# создаем таблицы
-metadata.create_all(bind=engine)
+from .base import metadata, engine
--- src/main.py
+++ src/main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
+from contextlib import asynccontextmanager
 
-from db.base import database
+from db.base import database, engine
+from db.tables import metadata
 from handlers import menu, sub_menu, dish
 
 
-app = FastAPI(title='YLAB MENU')
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Создаем таблицы в базе данных при старте сервера
+    metadata.create_all(engine)
+    await database.connect()
+    print('Database connected')
+    yield
+    await database.disconnect()
+    print('Database disconnected')
+
+app = FastAPI(title='MENU', lifespan=lifespan)
 
 app.include_router(menu.router, prefix='/api/v1/menus', tags=['menus'])
 app.include_router(sub_menu.router, prefix='/api/v1/menus', tags=['submenus'])
 app.include_router(dish.router, prefix='/api/v1/menus', tags=['dishs'])
-
-@app.on_event('startup')
-async def startup():
-    await database.connect()
-
-@app.on_event('shutdown')
-async def shutdown():
-    await database.disconnect()
